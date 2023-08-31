d4236ad22da2b40c7ea9b345dde637858a45a1f2
ryansu <phantacix@gmail.com>
2023-08-31 13:24:50 +0800

    update: patch1.go

--- patch/patch1.go
+++ patch/patch1.go
@@ -2,8 +2,6 @@ package patch
 
 import "github.com/cherry-game/cherry-hotfix/model"
 
-func FixFooHello() func(foo *model.Foo) string {
-	return func(foo *model.Foo) string {
-		return "foo.Hello() is fixed"
-	}
+func FixFooHello(foo *model.Foo) string {
+	return "func is fixed"
 }
