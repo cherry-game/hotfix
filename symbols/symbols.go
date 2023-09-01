1e1cd0dd53ae10981460419ee6af9e7161a87d87
ryansu <phantacix@gmail.com>
2023-09-01 11:39:34 +0800

    update: clean code

--- symbols/symbols.go
+++ symbols/symbols.go
@@ -3,6 +3,7 @@ package symbols
 import (
 	"reflect"
 
+	"github.com/cherry-game/cherry-hotfix/hotfix"
 	"github.com/traefik/yaegi/stdlib"
 )
 
@@ -12,8 +13,12 @@ func init() {
 	for k, v := range stdlib.Symbols {
 		Symbols[k] = v
 	}
+
+	Symbols["github.com/cherry-game/cherry-hotfix/hotfix/hotfix"] = map[string]reflect.Value{
+		// type definitions
+		"FuncPatch": reflect.ValueOf((*hotfix.FuncPatch)(nil)),
+	}
 }
 
 // 点击生成符号表
-//go:generate yaegi extract github.com/cherry-game/cherry-hotfix/hotfix hotfix
 //go:generate yaegi extract github.com/cherry-game/cherry-hotfix/test/model model
